156841425de4f631ad5afd06c4ab1fe0cb60e6fc
Loki-1601 <potlurilokeshwari@gmail.com>
2024-08-22 22:41:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from io import StringIO
 
 # Streamlit title and description
 st.title("Hourly Wage Trends in Canada Over Time by Occupation")
-st.write("""
-This visualization compares the overall hourly wage trend of all occupations with those of engineering and health professionals in Canada.
-""")
 
 # Load the data
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
