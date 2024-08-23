d54fa241382f5c61720adbe26a11ee90c715247f
Loki-1601 <potlurilokeshwari@gmail.com>
2024-08-22 22:47:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,20 @@ import matplotlib.pyplot as plt
 from io import StringIO
 
 # Streamlit title and description
-#st.title("Hourly Wage Trends in Canada Over Time by Occupation")
+st.title("Hourly Wage Trends in Canada Over Time by Occupation")
 
 # Load the data
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
+#uploaded_file = st.file_uploader("Choose a file")
+#if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    #dataframe = pd.read_csv(uploaded_file)
+    #st.write(dataframe)
+
+
+uploaded_files = st.file_uploader(
+    "Choose a CSV file", accept_multiple_files=True
+)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
